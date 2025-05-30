8be30eba59b18ebdae708551825ff5f9262071b0
heliel <helielwilliam13@gmail.com>
2025-05-30 20:33:10 -0300

    Testes

---
+++ smattv/src/SmartTv.java
@@ -0,0 +1,15 @@
+public class SmartTv {
+
+    boolean ligado = false;
+    int canal = 1;
+    int volume = 25;
+
+    public void ligar(){
+        ligado = true;
+
+    }
+    
+    public void desligar(){
+        ligado = false;
+    }
+}
