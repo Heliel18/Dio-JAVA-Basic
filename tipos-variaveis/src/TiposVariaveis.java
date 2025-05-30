e07cc67bbd1d385db9304be4b3544d4d45893983
heliel <helielwilliam13@gmail.com>
2025-05-30 19:53:01 -0300

    Estudos de tipos de variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,25 @@
 public class TiposVariaveis{
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        String meuNome = "Heliel";
+        
+        double salario = 2500.44;
+
+        short numeroCurto = 1;
+        
+        int numeroNormal = numeroCurto;
+
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 60;
+
+        numero = 30;
+
+        System.out.println(numero);
+
+        final double PI = 3.14;
+
+        
+
     }
 }
