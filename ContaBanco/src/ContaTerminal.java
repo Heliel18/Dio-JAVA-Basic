c203308390fa9a060119aeb688f996ad7ade9639
heliel <helielwilliam13@gmail.com>
2025-05-31 14:35:20 -0300

    Isso

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -2,35 +2,11 @@ import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) {
-
-        System.out.println("Bem vindo, vamos criar sua conta no nosso banco ?!");
-        System.out.println("");
-        Scanner resposta = new Scanner(System.in);
-
-        System.out.println("Digite o numero da sua conta: ");
-
-        int numeroConta = resposta.nextInt();
-
-        System.out.println("Agora a sua agencia: ");
-
-        String agencia = resposta.next();
-
-        System.out.println("Seu nome completo: ");
-
-        String nomeCompleto = resposta.next();
-
-        System.out.println("Qual seu saldo: ");
-
-        double saldoConta = resposta.nextDouble();
-
-        System.out.println("Olá " + nomeCompleto + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo " + saldoConta + " já está disponível para saque!");
-
-
-
-
-
-
+        int numero = 1;
+        for (int x = 1; x < 2; x++) {
+            numero = numero + x;
+        }
+        System.out.println("O valor de número é: " + numero);
     }
 
-      
 }
